5dabda8cd2b09d278559ea6ccc61526dc95bb149
MatixTech404 <81434169+MatixTech404@users.noreply.github.com>
2023-02-13 20:20:35 +0100

    rename window to main and make methods that modify grid

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import pygame as pg
+import pygame.locals as pgc  # pygame constants
+
+from wgrid import WGrid
+import colors as c
+
+# our constants
+WIDTH, HEIGHT = 600, 600
+
+pg.init()
+WINDOW = pg.display.set_mode((WIDTH, HEIGHT))
+
+grid = WGrid(WINDOW, None, (0, 0), (600, 600), margin=1)
+
+run = True
+while run:
+    for e in pg.event.get():
+        if e.type == pgc.QUIT:
+            run = False
+        if e.type == pgc.MOUSEBUTTONDOWN:
+            if e.button == pgc.BUTTON_LEFT:
+                pos = e.pos
+                grid.toggle_cell(pos[0], pos[1])
+            if e.button == pgc.BUTTON_RIGHT:
+                grid.evolve()
+        if e.type == pgc.KEYDOWN:
+            if e.key == pgc.K_SPACE:
+                grid.evolve()
+            if e.key == pgc.K_c:
+                grid.set_grid()
+            if e.key == pgc.K_r:
+                grid.set_random_grid()
+
+    WINDOW.fill(c.BLACK)
+
+    grid.draw()
+
+    pg.display.update()
+
+pg.quit()
